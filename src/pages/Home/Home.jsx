84326275e858e834c75061d5a576eefc27e42b81
Logapriya <slogaapriya@gmail.com>
2023-08-11 12:02:15 +0530

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
 import Sidenav from "../../components/sidenav";
 
 export default function Home() {
-  const navigate = useNavigate();
   return (
     <div style={{ display: "flex" }}>
       <Sidenav />
